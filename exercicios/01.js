ab60737be65d6eb0a4e8a592df06d0e2937c2116
Kislan <kislan.lima@escolar.ifrn.edu.br>
2024-03-17 15:15:20 -0300

    fim01

--- exercicios/01.js
+++ exercicios/01.js
@@ -1,16 +1,18 @@
 function calcular(){
-    let dia = document.getElementById("dia")
-    let mes = document.getElementById("mes")
-    let ano = document.getElementById("ano")
-    let res = document.getElementById("res")
-    let data = new Date()
-    let pano = data.getFullYear()
-    let pmes = data.getMonth
-    let pdia = data.getDay()
-    
-    idadeano = pano - ano 
-    
-     
+    let data = document.getElementById("pop").value;
+    let dataNas = new Date(data);
+    let res = document.getElementById("res");
+
+    let hoje = new Date();
+
+    let idade = hoje.getFullYear() - dataNas.getFullYear();
+    let mes = hoje.getMonth() - dataNas.getMonth();
+
+    if (mes < 0 || (mes === 0 && hoje.getDate() < dataNas.getDate())) {
+        idade--;
+    }
+
+    res.innerHTML = `Idade: ${idade} anos`;
 }
 
 // <!-- Crie uma função que calcule a idade de uma pessoa com base na data de nascimento fornecida em um campo de formulário. -->
