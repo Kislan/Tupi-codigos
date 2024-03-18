a171b93e1b43fd8298a4ad35ce6a533d78c3b315
Kislan <kislan.lima@escolar.ifrn.edu.br>
2024-03-18 01:26:57 -0300

    ex 04 incompleto

---
+++ ex04/04.js
@@ -0,0 +1,10 @@
+function tome(){
+    let email = document.getElementById("em")
+    
+
+
+    if(email.includes('@') && email.includes('.')){
+        return alert(`Seu email está correto`)
+    }else{
+        return alert("sfre")
+    }}
