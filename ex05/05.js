6be2153b38f9b7cd801be9419909a3dd9ef3a63c
Kislan <kislan.lima@escolar.ifrn.edu.br>
2024-03-18 19:40:27 -0300

    ex05 feito

--- ex05/05.js
+++ ex05/05.js
@@ -1,9 +1,18 @@
 function pesquisar(){
-    let text = document.getElementById("ti")
-    let letra = document.getElementById("ri")
+    let text = document.getElementById("ti").value
+    let letra = document.getElementById("ri").value
     let res = document.getElementById("res")
+    let comprimento = text.length
+    let contar = 0
 
     if (text.includes(letra)){
-        res.innerHTML = (`A letra ${letra} aparece `)
+        for(let i = 0;i<comprimento;i++)
+            if(text[i] === letra){
+                contar++
+            }
+
+        res.innerHTML = `A letra: "${letra}" Aparece ${contar} vezes`
+    }else{
+        res.innerHTML = `A letra: "${letra}" Não foi encontrada`
     }
 }
