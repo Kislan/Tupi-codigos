45910101ed84bbf8948b191785c09856520d5467
Kislan <kislan.lima@escolar.ifrn.edu.br>
2024-03-19 19:19:18 -0300

    ex06 feito +

---
+++ ex06/06.js
@@ -0,0 +1,26 @@
+function troca(){
+    let cor = document.getElementById("lol").value.toLowerCase()
+    let text = document.getElementById("text")
+
+    if (cor == "vermelho"){
+        text.style.color = "red"
+    }else if (cor == "azul"){
+        text.style.color = "blue"
+    }else if (cor == "amarelo"){
+        text.style.color = "yellow"
+    }else if (cor == "rosa"){
+        text.style.color = "pink"
+    }else if (cor == "roxo"){
+        text.style.color = "purple"
+    }else if (cor == "laranja"){
+        text.style.color = "orange"
+    }else if (cor == "marrom"){
+        text.style.color = "brown"
+    }else if (cor == "verde"){
+        text.style.color = "green"
+    }else if (cor == "preto"){
+        alert("A letra já é preta Abençoado!")
+    }else {
+        alert("Escolha uma cor comum!")
+    }
+}
