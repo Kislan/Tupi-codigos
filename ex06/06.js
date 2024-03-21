a499dd89846bb532bc49f5dbceda2633edc57e69
Kislan <kislan.lima@escolar.ifrn.edu.br>
2024-03-21 15:29:19 -0300

    2.0

--- ex06/06.js
+++ ex06/06.js
@@ -1,26 +1,5 @@
 function troca(){
-    let cor = document.getElementById("lol").value.toLowerCase()
-    let text = document.getElementById("text")
+    let cor = document.getElementById("cores").value
+    let text = document.getElementById("text").style.color = cor
 
-    if (cor == "vermelho"){
-        text.style.color = "red"
-    }else if (cor == "azul"){
-        text.style.color = "blue"
-    }else if (cor == "amarelo"){
-        text.style.color = "yellow"
-    }else if (cor == "rosa"){
-        text.style.color = "pink"
-    }else if (cor == "roxo"){
-        text.style.color = "purple"
-    }else if (cor == "laranja"){
-        text.style.color = "orange"
-    }else if (cor == "marrom"){
-        text.style.color = "brown"
-    }else if (cor == "verde"){
-        text.style.color = "green"
-    }else if (cor == "preto"){
-        text.style.color = "green"
-    }else {
-        alert("Escolha uma cor comum!")
-    }
 }
