15bce5af402d5e22366d297d4fe588f60d42280b
Kislan <kislan.lima@escolar.ifrn.edu.br>
2024-03-22 07:17:17 -0300

    ref

--- ex10/10.js
+++
@@ -1,8 +0,0 @@
-let ordem = []
-function cres(){
-    let numero = document.getElementById("num1").value
-    let res = document.getElementById("res")
-    ordem.push(numero)
-    res.innerHTML = `Ordem Crescente: ${ordem.sort(ordem)}`
-
-}
